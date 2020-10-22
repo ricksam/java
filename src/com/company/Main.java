27ccd31902b68123c0783913024627bd29977bdd
Ricardo Sampaio <ricardo.sampaio@totvspartners.com.br>
2020-10-22 11:23:07 -0300

    start

---
+++ src/com/company/Main.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import com.sun.deploy.net.HttpRequest;
+import sun.net.www.http.HttpClient;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import com.company.HttpsTrustManager;
+
+public class Main {
+
+    //private static HttpsURLConnection connection;
+
+    public static void main(String[] args) {
+        com.company.HttpsTrustManager.allowAllSSL();
+        // Create a neat value object to hold the URL
+
+        try {
+            URL url = new URL("https://bluecore-metodo-blobs.azurewebsites.net/api/RequestPermission");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestProperty("accept", "application/json");
+            connection.setRequestMethod("GET");
+            InputStream responseStream = responseStream = connection.getInputStream();
+            System.out.println(responseStream.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
